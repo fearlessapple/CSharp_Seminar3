82e7c8353ac081c471c396ab48d331cda8377a8f
Stas <smaster950@gmail.com>
2022-10-12 20:53:53 +0300

    Complete Exercise23

--- Exercise23/Program.cs
+++ Exercise23/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{N} -> ");
+PrintSquares(N);
+
+
+static void PrintSquares(int N)
+{
+    for(int i = 1; i < N; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+    Console.Write($"{N*N*N}");
+}
