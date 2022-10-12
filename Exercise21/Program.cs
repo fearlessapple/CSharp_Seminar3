79fc5beb117d41787810d3ee4e7059c7eb4a664a
Stas <smaster950@gmail.com>
2022-10-12 20:50:00 +0300

    Complete Exercise21

--- Exercise21/Program.cs
+++ Exercise21/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A1, A2, B1, B2, C1, C2;
+
+A1 = UserInputCoordinate("Введите координату Х первого числа: ");
+Console.WriteLine(A1);
+A2 = UserInputCoordinate("Введите координату Y первого числа: ");
+Console.WriteLine(A2);
+C1 = UserInputCoordinate("Введите координату Z первого числа: ");
+Console.WriteLine(C1);
+B1 = UserInputCoordinate("Введите координату Х второго числа: ");
+Console.WriteLine(B1);
+B2 = UserInputCoordinate("Введите координату Y второго числа: ");
+Console.WriteLine(B2);
+C2 = UserInputCoordinate("Введите координату Z второго числа: ");
+Console.WriteLine(C2);
+
+double distance = Distance(A1, A2, B1, B2, C1, C2);
+Console.WriteLine(distance);
+
+static int UserInputCoordinate(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine() ?? "");
+    return result;
+}
+
+static double Distance(int A1, int A2, int B1, int B2, int C1, int C2)
+{
+    double result = Math.Sqrt(Math.Pow(B1 - A1, 2) + Math.Pow(B2 - A2, 2) + Math.Pow(C2 - C1, 2));
+    return result;
+}
