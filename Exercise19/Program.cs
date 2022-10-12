e965fa79deed09720c0e5a83bf7d9d012b21d5c7
Stas <smaster950@gmail.com>
2022-10-12 20:33:18 +0300

    Complete Exercise19

--- Exercise19/Program.cs
+++ Exercise19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+int LastNumber = N % 10;
+int PreLastNumber = (N / 10) % 10;
+int SecondNumber = (N / 1000) % 10;
+int FirstNumber = N / 10000;
+
+if(FirstNumber == LastNumber && SecondNumber == PreLastNumber)
+{
+    Console.WriteLine($"{N} -> да");
+}
+else
+{
+    Console.WriteLine($"{N} -> нет");
+}
